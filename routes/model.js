abdd1e4cbacd2b6d896c708bde366e31991c6111
batman1292 <batman12922@gmail.com>
2017-01-31 19:40:16 +0700

    create CRUD 3 table

--- routes/model.js
+++ routes/model.js
@@ -63,12 +63,12 @@ router.post('/checkHashMD5', function(req, res, next){
 });
 
 router.put('/updateRFIDCode',function(req, res, next){
-  var   query = "UPDATE STU_CARD SET RFID_CODE == '"+req.body.rfid_code+"' WHERE STU_CODE == '"+req.body.studentID+"'";
+  var query = "UPDATE STU_CARD SET RFID_CODE == '"+req.body.rfid_code+"' WHERE STU_CODE == '"+req.body.studentID+"'";
   console.log(query);
   db.run(query, [],
     function(err) {
       if (err) {
-        res.status(404).json({error: "Not Found"});
+        res.status(400).json({error: "Can't update data"});
       }else{
         res.status(200).json({msg: "success"});
       }
@@ -76,6 +76,202 @@ router.put('/updateRFIDCode',function(req, res, next){
   );
 });
 
-// router.get('/')
+// CRUD for STU_INFO table
+router.post('/studentInfo', function(req, res, next){
+  var query = "INSERT INTO STU_INFO (CODE, TITLE, NAME, SURNAME) VALUES ('"+req.body.studentID+"', '"+req.body.title+"', '"+req.body.name+"', '"+req.body.surname+"')";
+  db.run(query, [],
+    function(err) {
+      if (err) {
+        res.status(400).json({error: "Can't create data"});
+      }else{
+        res.status(200).json({msg: "success"});
+      }
+    }
+  );
+});
+
+// get all student
+router.get('/studentInfo', function(req, res, next){
+  var query = "SELECT * FROM STU_INFO";
+  db.get(query,
+    function(err, row){
+      if(err){
+        res.status(500).json({error: "db error"});
+      }else{
+        if(row != null){
+          res.status(200).json({data: row});
+        }else{
+          res.status(404).json({error: "Not Found"});
+        }
+      }
+    }
+  );
+});
+// get from studentID
+router.get('/studentInfo/:studentID', function(req, res, next){
+  var query = "SELECT * FROM STU_INFO WHERE STU_CODE == '"+req.params.studentID+"'";
+  db.get(query,
+    function(err, row){
+      if(err){
+        res.status(500).json({error: "db error"});
+      }else{
+        if(row != null){
+          res.status(200).json({data: row});
+        }else{
+          res.status(404).json({error: "Not Found"});
+        }
+      }
+    }
+  );
+});
+
+router.put('/studentInfo', function(req, res, next){
+  // var query = "UPDATE "
+});
+
+router.delete('/studentInfo', function(req, res, next){
+  var query = "DELETE FROM STU_INFO WHERE CODE = '"+req.body.studentID+"'";
+  db.run(query, [],
+    function(err) {
+      if (err) {
+        res.status(400).json({error: "Can't create data"});
+      }else{
+        res.status(200).json({msg: "success"});
+      }
+    }
+  );
+});
+
+// CRUD for STU_CARD table
+router.post('/studentCard', function(req, res, next){
+  var query = "INSERT INTO STU_CARD (STU_CODE, RFID_CODE) VALUES ('"+req.body.studentID+"', '"+req.body.rfidID+"')";
+  db.run(query, [],
+    function(err) {
+      if (err) {
+        res.status(400).json({error: "Can't create data"});
+      }else{
+        res.status(200).json({msg: "success"});
+      }
+    }
+  );
+});
+
+// get all student
+router.get('/studentCard', function(req, res, next){
+  var query = "SELECT * FROM STU_CARD";
+  db.get(query,
+    function(err, row){
+      if(err){
+        res.status(500).json({error: "db error"});
+      }else{
+        if(row != null){
+          res.status(200).json({data: row});
+        }else{
+          res.status(404).json({error: "Not Found"});
+        }
+      }
+    }
+  );
+});
+// get from studentID
+router.get('/studentCard/:studentID', function(req, res, next){
+  var query = "SELECT * FROM STU_CARD WHERE STU_CODE == '"+req.params.studentID+"'";
+  db.get(query,
+    function(err, row){
+      if(err){
+        res.status(500).json({error: "db error"});
+      }else{
+        if(row != null){
+          res.status(200).json({data: row});
+        }else{
+          res.status(404).json({error: "Not Found"});
+        }
+      }
+    }
+  );
+});
+
+router.put('/studentCard', function(req, res, next){
+  // var query = "UPDATE "
+});
+
+router.delete('/studentCard', function(req, res, next){
+  var query = "DELETE FROM STU_CARD WHERE STU_CODE = '"+req.body.studentID+"'";
+  db.run(query, [],
+    function(err) {
+      if (err) {
+        res.status(400).json({error: "Can't create data"});
+      }else{
+        res.status(200).json({msg: "success"});
+      }
+    }
+  );
+});
+
+// CRUD for STU_HASH table
+router.post('/studentHash', function(req, res, next){
+  var query = "INSERT INTO STU_HASH (STU_CODE, SALT, ID_MD5, ID_SHA256) VALUES ('"+req.body.studentID+"', '"+req.body.salt+"', '"+req.body.md5+"', '"+req.body.sha256+"')";
+  db.run(query, [],
+    function(err) {
+      if (err) {
+        res.status(400).json({error: "Can't create data"});
+      }else{
+        res.status(200).json({msg: "success"});
+      }
+    }
+  );
+});
+
+// get all student
+router.get('/studentHash', function(req, res, next){
+  var query = "SELECT * FROM STU_HASH";
+  db.get(query,
+    function(err, row){
+      if(err){
+        res.status(500).json({error: "db error"});
+      }else{
+        if(row != null){
+          res.status(200).json({data: row});
+        }else{
+          res.status(404).json({error: "Not Found"});
+        }
+      }
+    }
+  );
+});
+// get from studentID
+router.get('/studentHash/:studentID', function(req, res, next){
+  var query = "SELECT * FROM STU_HASH WHERE STU_CODE == '"+req.params.studentID+"'";
+  db.get(query,
+    function(err, row){
+      if(err){
+        res.status(500).json({error: "db error"});
+      }else{
+        if(row != null){
+          res.status(200).json({data: row});
+        }else{
+          res.status(404).json({error: "Not Found"});
+        }
+      }
+    }
+  );
+});
+
+router.put('/studentHash', function(req, res, next){
+  // var query = "UPDATE "
+});
+
+router.delete('/studentHash', function(req, res, next){
+  var query = "DELETE FROM STU_HASH WHERE STU_CODE = '"+req.body.studentID+"'";
+  db.run(query, [],
+    function(err) {
+      if (err) {
+        res.status(400).json({error: "Can't create data"});
+      }else{
+        res.status(200).json({msg: "success"});
+      }
+    }
+  );
+});
 
 module.exports = router;
